2a9dbbecb7b8778995aa4ef9e4f316a5940aa2a9
wedname <vvv.wedname@gmail.com>
2021-03-23 18:53:25 +0200

    Initial commit

--- main.py
+++ main.py
@@ -3,96 +3,105 @@
 (массив из пяти элементов). Создать массив из десяти элементов такого типа, упорядочить записи по возрастанию среднего
 балла. Добавить возможность вывода фамилий и номеров групп студентов, имеющих оценки, равные только 4 или 5.
 """
-import re
-
-
-class Student:
-    def __init__(self, name: str, group_number: str, grades: list):
-        self.name = name
-        self.group_number = group_number
-        self.grades = grades
-
-    regular_name = '[А-Я][а-я]{1,20}\s[А-Я]\.[А-Я]\.'
-    reg_name = re.compile(regular_name)
-
-    @property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def name(self, value, reg_name=reg_name):
-        if isinstance(value, str) and reg_name.findall(value):
-            self._name = value
-        else:
-            raise ValueError('name должен быть типом данных str и записан в формате Фамилия И.О.')
-
-    @property
-    def group_number(self):
-        return self._group_number
-
-    @group_number.setter
-    def group_number(self, group):
-        if isinstance(group, str):
-            self._group_number = group
-        else:
-            raise ValueError('group_number должен быть типом данных str')
-
-    @property
-    def grades(self):
-        return self._grades
-
-    @grades.setter
-    def grades(self, value):
-        if isinstance(value, list) and len(value) == 5:
-            self._grades = value
-        else:
-            raise ValueError('grades должен быть типом данных list с длиной 5')
-
-    def __lt__(self, other):
-        return sum(self.grades) < sum(other.grades)
-
-
-def show_students():
-    for j in range(len(students)):
-        print(f"ФИО: {students[j].name}, "
-              f"Группа: {students[j].group_number}, "
-              f"Оценки: {[x for x in students[j].grades]}\n")
-
-
-def sort_by_average_score():
-    students.sort()
-    show_students()
-
-
-def show_good_students():
-    for j in students:
-        if (1, 2, 3) not in j.grades:
-            print(f"{j.name}")
-
-
-students = []
-
-if __name__ == '__main__':
-    for i in range(0, 4):
-        students_name = input(f'Введите для {i+1}-го ученика ФИО: ')
-        students_group = input(f"Введите для {i+1}-го ученика группу: ")
-        students_grades = [int(input(f'Введите {x+1}-ую оценку от 1 до 5: ')) for x in range(0, 5)]
-        students.append(Student(students_name, students_group, students_grades))
-
-    show_students()
-
-    while True:
-        controls = input("1 - упорядочить записи по возрастанию среднего балла учеников\n"
-                         "2 - показать учеников, у которых оценки 4, 5\n"
-                         "0 - выйти из программы\n-->")
-        if controls == '1':
-            sort_by_average_score()
-        elif controls == '2':
-            show_good_students()
-        elif controls == '0':
-            exit()
-        else:
-            print("Нет такого действия!")
+# import re
+# from typing import List
+#
+#
+# class Student:
+#     def __init__(self, name: str, group_number: str, grades: List[int]):
+#         self.name = name
+#         self.group_number = group_number
+#         self.grades = grades
+#
+#     regular_name = '[А-Я][а-я]{1,20}\s[А-Я]\.[А-Я]\.'
+#     reg_name = re.compile(regular_name)
+#
+#     @property
+#     def name(self):
+#         return self._name
+#
+#     @name.setter
+#     def name(self, value, reg_name=reg_name):
+#         if isinstance(value, str) and reg_name.findall(value):
+#             self._name = value
+#         else:
+#             raise ValueError('name должен быть типом данных str и записан в формате Фамилия И.О.')
+#
+#     @property
+#     def group_number(self):
+#         return self._group_number
+#
+#     @group_number.setter
+#     def group_number(self, group):
+#         if isinstance(group, str):
+#             self._group_number = group
+#         else:
+#             raise ValueError('group_number должен быть типом данных str')
+#
+#     @property
+#     def grades(self):
+#         return self._grades
+#
+#     @grades.setter
+#     def grades(self, values):
+#         if isinstance(values, list) and len(values) == 5:
+#             for value in values:
+#                 if 0 < value < 6:
+#                     self._grades = values
+#                 else:
+#                     raise ValueError('grades must be 1-5')
+#         else:
+#             raise ValueError('grades должен быть типом данных list с длиной 5')
+#
+#     def __lt__(self, other):
+#         return sum(self.grades) < sum(other.grades)
+#
+#
+# def show_students():
+#     for j in range(len(students)):
+#         print(f"ФИО: {students[j].name}, "
+#               f"Группа: {students[j].group_number}, "
+#               f"Оценки: {[x for x in students[j].grades]}\n")
+#
+#
+# def sort_by_average_score():
+#     students.sort()
+#     show_students()
+#
+#
+# def show_good_students():
+#     bad_grades = [1, 2, 3]
+#     for j in students:
+#         for value in j.grades:
+#             if value in bad_grades:
+#                 break
+#         else:
+#             print(f"{j.name}")
+#
+#
+# students = []
+#
+# if __name__ == '__main__':
+#     for i in range(0, 4):
+#         students_name = input(f'Введите для {i+1}-го ученика ФИО: ')
+#         students_group = input(f"Введите для {i+1}-го ученика группу: ")
+#         students_grades = [int(input(f'Введите {x+1}-ую оценку от 1 до 5: ')) for x in range(0, 5)]
+#         students.append(Student(students_name, students_group, students_grades))
+#
+#     show_students()
+#
+#     while True:
+#         controls = input("1 - упорядочить записи по возрастанию среднего балла учеников\n"
+#                          "2 - показать учеников, у которых оценки 4, 5\n"
+#                          "0 - выйти из программы\n-->")
+#         if controls == '1':
+#             sort_by_average_score()
+#         elif controls == '2':
+#             show_good_students()
+#         elif controls == '0':
+#             exit()
+#         else:
+#             print("Нет такого действия!")
 
 
 """
@@ -103,92 +112,92 @@ if __name__ == '__main__':
 """
 
 
-# class Train:
-#     def __init__(self, destination, train_number, departure_time):
-#         self.destination = destination
-#         self.train_number = train_number
-#         self.departure_time = departure_time
-#
-#     @property
-#     def destination(self):
-#         return self._destination
-#
-#     @destination.setter
-#     def destination(self, value):
-#         if isinstance(value, str):
-#             self._destination = value
-#         else:
-#             raise ValueError('destination должен быть типом данных str')
-#
-#     @property
-#     def train_number(self):
-#         return self._train_number
-#
-#     @train_number.setter
-#     def train_number(self, value):
-#         if isinstance(value, int):
-#             self._train_number = value
-#         else:
-#             raise ValueError('train_number должен быть типом данных int')
-#
-#     @property
-#     def departure_time(self):
-#         return self._departure_time
-#
-#     @departure_time.setter
-#     def departure_time(self, value):
-#         if isinstance(value, str):
-#             self._departure_time = value
-#         else:
-#             raise ValueError('departure_time должен быть типом данных str')
-#
-#     def __lt__(self, other):
-#         return self.train_number < other.train_number
-#
-#
-# trains = []
-#
-#
-# def sort_by_train_number():
-#     trains.sort()
-#     for j in range(len(trains)):
-#         print(f"Номер поезда: {trains[j].train_number}, "
-#               f"отправка: {trains[j].departure_time}, "
-#               f"пункт назначения: {trains[j].destination}\n")
-#
-#
-# if __name__ == '__main__':
-#     for i in range(0, 5):
-#         trains_destination = input(f'Введите для {i}-го поезда пункт назначения: ')
-#         trains_number = int(input(f'Введите для {i}-го поезда его номер: '))
-#         trains_departure_time = input(f'Введите для {i}-го поезда время отправления: ')
-#         trains.append(Train(trains_destination, trains_number, trains_departure_time))
-#
-#     for i in range(len(trains)):
-#         print(f"Номер поезда: {trains[i].train_number}, "
-#               f"отправка: {trains[i].departure_time}, "
-#               f"пункт назначения: {trains[i].destination}\n")
-#
-#     while True:
-#         controls = input("1 - посмотреть информацию о поезде\n"
-#                          "2 - отсортировать поезда по номерам\n"
-#                          "3 - отсортировать поезда по пунктам назначения\n"
-#                          "0 - выйти из программы\n-->")
-#         if controls == '1':
-#             number_of_train = input("Введите номер поезда, чтоб узнать о нем информацию: ")
-#             for i in range(len(trains)):
-#                 if trains[i] == int(number_of_train):
-#                     print(f"Номер поезда: {trains[i].train_number}, "
-#                           f"отправка: {trains[i].departure_time}, "
-#                           f"пункт назначения: {trains[i].destination}\n")
-#         elif controls == '2':
-#             sort_by_train_number()
-#         elif controls == '3':
-#             pass
-#         elif controls == '0':
-#             exit()
-#         else:
-#             print("Нет такого действия!")
+class Train:
+    def __init__(self, destination, train_number, departure_time):
+        self.destination = destination
+        self.train_number = train_number
+        self.departure_time = departure_time
+
+    @property
+    def destination(self):
+        return self._destination
+
+    @destination.setter
+    def destination(self, value):
+        if isinstance(value, str):
+            self._destination = value
+        else:
+            raise ValueError('destination должен быть типом данных str')
+
+    @property
+    def train_number(self):
+        return self._train_number
+
+    @train_number.setter
+    def train_number(self, value):
+        if isinstance(value, int):
+            self._train_number = value
+        else:
+            raise ValueError('train_number должен быть типом данных int')
+
+    @property
+    def departure_time(self):
+        return self._departure_time
+
+    @departure_time.setter
+    def departure_time(self, value):
+        if isinstance(value, str):
+            self._departure_time = value
+        else:
+            raise ValueError('departure_time должен быть типом данных str')
+
+    def __lt__(self, other):
+        return self.train_number < other.train_number
+
+
+trains = []
+
+
+def sort_by_train_number():
+    trains.sort()
+    for j in range(len(trains)):
+        print(f"Номер поезда: {trains[j].train_number}, "
+              f"отправка: {trains[j].departure_time}, "
+              f"пункт назначения: {trains[j].destination}\n")
+
+
+if __name__ == '__main__':
+    for i in range(0, 5):
+        trains_destination = input(f'Введите для {i}-го поезда пункт назначения: ')
+        trains_number = int(input(f'Введите для {i}-го поезда его номер: '))
+        trains_departure_time = input(f'Введите для {i}-го поезда время отправления: ')
+        trains.append(Train(trains_destination, trains_number, trains_departure_time))
+
+    for i in range(len(trains)):
+        print(f"Номер поезда: {trains[i].train_number}, "
+              f"отправка: {trains[i].departure_time}, "
+              f"пункт назначения: {trains[i].destination}\n")
+
+    while True:
+        controls = input("1 - посмотреть информацию о поезде\n"
+                         "2 - отсортировать поезда по номерам\n"
+                         "3 - отсортировать поезда по пунктам назначения\n"
+                         "0 - выйти из программы\n-->")
+        if controls == '1':
+            number_of_train = input("Введите номер поезда, чтоб узнать о нем информацию: ")
+            for i in range(len(trains)):
+                if trains[i] == int(number_of_train):
+                    print(f"Номер поезда: {trains[i].train_number}, "
+                          f"отправка: {trains[i].departure_time}, "
+                          f"пункт назначения: {trains[i].destination}\n")
+        elif controls == '2':
+            sort_by_train_number()
+        elif controls == '3':
+            pass
+        elif controls == '0':
+            exit()
+        else:
+            print("Нет такого действия!")
 
 """
 Задание #3:
